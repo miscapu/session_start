f588f28598ded63dce4eec071deee8b130c1abb2
miscapu <miguelonsinho5@gmail.com>
2018-07-28 22:41:38 -0300

    Starting session with session_start()

---
+++ page2.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @Author: Miguel Santisteban
+ * @WebSite: mysqliphp.blogspot.com
+ * @File:page2.php
+ * @Project: Course PHP: Using session_start in PHP 7
+ * */
+//Starting session with session_start
+session_start();
