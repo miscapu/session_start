b3c6df2707c409b67ed44c90a8a858dc886e177f
miscapu <miguelonsinho5@gmail.com>
2018-07-29 01:09:09 -0300

    Modificando el contenido de htmlspecialchars de SID para session_id(); sino no funciona!

--- page1.php
+++ page1.php
@@ -16,4 +16,4 @@ $_SESSION['time'] = time();
 //Works if session cookie was accepted
 echo '<br /> <a href="page2.php">Page 2</a>';
 //Or maybe pass along the session id, if needed
-echo '<br /> <a href="page2.php"?' . SID . '">Page 2</a>';
+echo '<br /> <a href="page2.php?' . session_name() . ' =' . session_id() . '">Page 2</a>';
