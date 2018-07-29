514ba6a612e8ffabafc1afee878aefb6751e6b98
miscapu <miguelonsinho5@gmail.com>
2018-07-28 22:21:20 -0300

    Starting session and show welcome page 1

---
+++ page1.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * @Author: Miguel Santisteban
+ * @WebSite: mysqliphp.blogspot.com
+ * @File:page1.php
+ * @Project: Course PHP: Using session_start in PHP 7
+ * */
+//Starting Session with session_start
+session_start();
+//Show in Screen 'Welcome to page #1'
+echo "Welcome to page #1";
