96984e30ef1414e5674823860099a77fcad65671
miscapu <miguelonsinho5@gmail.com>
2018-07-28 22:50:59 -0300

    Show on the screen the data entered in the superglobal session from page1.php

--- page2.php
+++ page2.php
@@ -9,3 +9,8 @@
 session_start();
 //Show in display "Welcome to page #2"
 echo 'Welcome to Page #2';
+//show on the screen the data entered in the superglobal session from page1.php
+echo $_SESSION['favcolor'];
+echo $_SESSION['animal'];
+echo date('Y m d H:i:s', $_SESSION['time']);
+
