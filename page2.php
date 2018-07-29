fd59d0698351fd0cb0172baccaf6f60db91b72b6
miscapu <miguelonsinho5@gmail.com>
2018-07-28 22:52:58 -0300

    Use SID here, like we did in page1.php

--- page2.php
+++ page2.php
@@ -13,4 +13,6 @@ echo 'Welcome to Page #2';
 echo $_SESSION['favcolor'];
 echo $_SESSION['animal'];
 echo date('Y m d H:i:s', $_SESSION['time']);
+//You may want to use SID here, like we did in page1.php
+echo '<br /><a href="page1.php">Page 1</a>';
 
